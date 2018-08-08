b4a13486cd4011e0f759fe7a6a3c9e58fde31c43
Phil Brockwell <phil.l.brockwell@gmail.com>
2018-08-08 15:39:34 +0100

    Initialize rails app with rspec, database cleaner and factory bot for testing

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+
+Bundler.require(*Rails.groups)
+
+module Cygnetise
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.generators.system_tests = nil
+  end
+end
