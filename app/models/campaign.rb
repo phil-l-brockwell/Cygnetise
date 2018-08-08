b5a635301c7f5f83521358e2b4b198e341825be0
Phil Brockwell <phil.l.brockwell@gmail.com>
2018-08-08 21:42:05 +0100

    Add Campaign.with_valid_votes and #valid_votes_grouped_by_choice_with_counts to use in views

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,4 +4,10 @@ class Campaign < ApplicationRecord
   has_many :votes
 
   validates :title, presence: true
+
+  scope :with_valid_votes, -> { joins(:votes).merge(Vote.valid).uniq }
+
+  def valid_votes_grouped_by_choice_with_counts
+    votes.valid.group(:choice).count
+  end
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -17,4 +17,33 @@ describe Campaign do
       end
     end
   end
+
+  describe '.with_valid_votes' do
+    let(:valid_campaign) { FactoryBot.create(:campaign) }
+    let!(:valid_vote) { FactoryBot.create(:vote, campaign: valid_campaign) }
+    let(:invalid_campaign) { FactoryBot.create(:campaign) }
+    let!(:invalid_vote) { FactoryBot.create(:vote, :invalid, campaign: invalid_campaign) }
+
+    it 'returns just the campaigns with valid votes' do
+      expect(described_class.with_valid_votes).to include(valid_campaign)
+      expect(described_class.with_valid_votes).not_to include(invalid_campaign)
+    end
+  end
+
+  describe '#valid_votes_grouped_by_choice_with_counts' do
+    subject { FactoryBot.build(:campaign) }
+    let(:votes) { subject.valid_votes_grouped_by_choice_with_counts }
+
+    before do
+      2.times { FactoryBot.create(:vote, campaign: subject) }
+      2.times { FactoryBot.create(:vote, campaign: subject, choice: 'Batman') }
+      2.times { FactoryBot.create(:vote, :invalid, campaign: subject, choice: 'Superman') }
+    end
+
+    it 'returns the valid_votes_grouped_by_choice_with_counts' do
+      expect(votes['Batman']).to eq(2)
+      expect(votes['Spiderman']).to eq(2)
+      expect(votes['Superman']).to eq(nil)
+    end
+  end
 end
